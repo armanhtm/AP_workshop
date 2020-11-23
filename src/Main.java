f7ea7657298440ccaae2593782773806dd937fea
Arman Hatami <armanhatami.aut@gmail.com>
2020-11-23 17:52:49 +0330

    final calculator with functionality

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 /**
  * @author Arman Hatami
  * @version 1.0
- * main for test our calculator
+ * main class to run the calculator
  */
 public class Main {
-
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         try {
             for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -18,5 +18,7 @@ public class Main {
             System.out.println("error");
         }
         Calculator calculator = new Calculator();
+
     }
 }
+
